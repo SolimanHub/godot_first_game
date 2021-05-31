2be9f8e37cf985fc873442d0fedcc7bf5ce7777d
solimanhub <david.soliman2010@gmail.com>
2021-05-31 14:48:18 -0600

    the game works, no animations on mob

--- Main.gd
+++ Main.gd
@@ -9,11 +9,15 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -21,6 +25,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.

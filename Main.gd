e3db41333f48b33758392210dd54667b21b60a03
solimanhub <david.soliman2010@gmail.com>
2021-05-31 19:00:52 -0600

    corregidos animaciones, add fondo, musica y conf enter

--- Main.gd
+++ Main.gd
@@ -11,6 +11,8 @@ func game_over():
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	get_tree().call_group("mobs", "queue_free")
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -18,6 +20,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
